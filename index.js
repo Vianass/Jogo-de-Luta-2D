2be64a10211c5b2a8d3b751bfe083e8cfb4c23dd
Diogo Viana <diogoviana2004@gmail.com>
2024-06-22 03:55:20 +0100

    sprite-run

--- index.js
+++ index.js
@@ -8,6 +8,24 @@ c.fillRect(0, 0, canvas.width, canvas.height)
 
 const gravity = 0.7
 
+const background = new Sprite({
+    position: {
+        x:0,
+        y:0
+    },
+    image5rc: './img/background.png'
+})
+
+const shop = new Sprite({
+    position: {
+        x:600,
+        y:128
+    },
+    image5rc: './img/shop.png',
+    scale: 2.75,
+    framesMax: 6
+})
+
 const player = new Fighter({
     position: {
     x: 0,
@@ -20,6 +38,23 @@ const player = new Fighter({
     offset: {
        x: 0,
        y: 0
+    },
+    image5rc: './img/samuraiMack/Idle.png',
+    framesMax: 8,
+    scale: 2.5,
+    offset: {
+        x: 215,
+        y: 157
+    },
+    sprites: {
+        idle: {
+            image5rc: './img/samuraiMack/Idle.png',
+            framesMax: 8
+        },
+        run: {
+            image5rc: './img/samuraiMack/Run.png',
+            framesMax: 8,
+        }
     }
 })
 
@@ -61,40 +96,6 @@ const keys = {
     }
 }
 
-function rectangularCollision({rectangle1,rectangle2}){
-    return (
-        rectangle1.attackBox.position.x + rectangle1.attackBox.width >= rectangle2.position.x && 
-        rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle2.width &&
-        rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y && 
-        rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height 
-    )
-}
-
-function determineWinner({player, enemy, timerId}){
-    clearTimeout(timerId)
-    document.querySelector('#displayText').style.display = 'flex'
-    if(player.health === enemy.health){
-        document.querySelector('#displayText').innerHTML = 'Tie'
-        }else if(player.health > enemy.health){
-        document.querySelector('#displayText').innerHTML = 'Player 1 Wins'
-        }else if(player.health < enemy.health){
-        document.querySelector('#displayText').innerHTML = 'Player 2 Wins'
-        }
-}
-
-let timer = 60
-let timerId
-function decreaseTimer(){
-    if (timer > 0) {
-        timerId = setTimeout(decreaseTimer, 1000)
-        timer--
-        document.querySelector('#timer').innerHTML = timer
-    }
-
-    if(timer === 0){
-        determineWinner({player, enemy, timerId})
-    }   
-}
 
 decreaseTimer()
 
@@ -102,17 +103,21 @@ function animate(){
     window.requestAnimationFrame(animate)
     c.fillStyle = 'black'
     c.fillRect(0, 0, canvas.width, canvas.height)
+    background.update()
+    shop.update()
     player.update()
-    enemy.update()
+    //enemy.update()
 
     player.velocity.x = 0
     enemy.velocity.x = 0
 
-
+    player.image = player.sprites.idle.image
     if (keys.a.pressed && player.lastKey === 'a'){
         player.velocity.x = -5
+        player.image = player.sprites.run.image
     }else if (keys.d.pressed && player.lastKey ==='d'){
         player.velocity.x = 5
+        player.image = player.sprites.run.image
     }
 
     if (keys.ArrowLeft.pressed && enemy.lastKey === 'ArrowLeft'){
--- js/classes.js
+++ js/classes.js
@@ -1,22 +1,51 @@
 class Sprite{
-    constructor({position, image5rc}){
+    constructor({position, image5rc, scale = 1, framesMax = 1, offset = {x:0, y:0} }){
         this.position = position
         this.width = 50
         this.height = 150
         this.image = new Image()
         this.image.src = image5rc
+        this.scale = scale
+        this.framesMax = framesMax
+        this.frameCurrent = 0
+        this.framesElapsed = 0
+        this.framesHold = 5
+        this.offset = offset
     }
     draw(){
-        c.drawImage(this.image, this.position.x, this.position.y)
+        c.drawImage(this.image, this.frameCurrent * (this.image.width / this.framesMax),0, this.image.width / this.framesMax, this.image.height,
+            this.position.x - this.offset.x, this.position.y - this.offset.y, (this.image.width / this.framesMax) * this.scale, this.image.height * this.scale)
     }
+
+    animateFrames(){
+        this.framesElapsed++
+
+        if(this.framesElapsed % this.framesHold === 0){
+            if(this.frameCurrent < this.framesMax - 1){
+            this.frameCurrent++
+             }else {
+            this.frameCurrent = 0
+            } 
+        }
+    }
+
     update(){
         this.draw()
+        this.animateFrames()
     }
 }
 
-class Fighter{
-    constructor({position, velocity, color = 'red', offset}){
-        this.position = position
+class Fighter extends Sprite {
+    constructor({position, velocity, color = 'red', image5rc, scale = 1, framesMax = 1, offset = {x:0, y:0}, sprites}){
+
+        super({
+            position,
+            image5rc,
+            scale,
+            framesMax,
+            offset
+        })
+
         this.velocity = velocity
         this.width = 50
         this.height = 150
@@ -33,27 +62,31 @@ class Fighter{
         this.color = color
         this.isAttacking
         this.health = 100
-    }
+        this.frameCurrent = 0
+        this.framesElapsed = 0
+        this.framesHold = 5
+        this.sprites = sprites
 
-    draw(){
-        c.fillStyle = this.color
-        c.fillRect(this.position.x, this.position.y, this.width, this.height)
-
-        if (this.isAttacking){
-        c.fillStyle = 'green'
-        c.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.width, this.attackBox.height)
+        for( const sprite in this.sprites){
+            sprites[sprite].image = new Image()
+            sprites[sprite].image.src = sprites[sprite].image5rc
         }
+        
     }
+
+
 
     update(){
         this.draw()
+        this.animateFrames()
+
         this.attackBox.position.x = this.position.x + this.attackBox.offset.x
         this.attackBox.position.y = this.position.y
          
         this.position.x += this.velocity.x
         this.position.y += this.velocity.y
 
-        if(this.position.y + this.height + this.velocity.y >= canvas.height) {
+        if(this.position.y + this.height + this.velocity.y >= canvas.height - 96) {
             this.velocity.y = 0
         }else this.velocity.y += gravity
     }
